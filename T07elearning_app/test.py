d37299787164bfd3f34fd216166438f14e3acda0
tunguyen-195 <tunguyen.work.private@gmail.com>
2025-02-16 20:36:17 +0700

    Đồng bộ

--- T07elearning_app/test.py
+++ T07elearning_app/test.py
@@ -1,9 +1,10 @@
-from app import db
-from app.models import User
+# create_temp_db.py
+from app import create_app
+from app.extensions import db
+from app.models import User, Role, Course, Enrollment, LectureSession, Assignment, Submission, Notification
 
-# Kiểm tra người dùng admin
-admin = User.query.filter_by(username='admin').first()
-print(admin)
-print(admin.email)
-print(admin.check_password('123'))  # Nên in ra True
-print(admin.check_password('wrongpassword'))  # Nên in ra False
+app = create_app()
+
+with app.app_context():
+    db.create_all()
+    # Bạn có thể thêm dữ liệu mẫu ở đây nếu cần
